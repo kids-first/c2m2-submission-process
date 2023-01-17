08ff22101b069c83503da6d066357b3d3c09efbe
William Khan <william@d3b.center>
2023-01-17 18:24:49 -0500

    Added script for acquiring osf tools.

---
+++ acquire_osf_c2m2_submission_tools.sh
@@ -0,0 +1,4 @@
+#osf -p bq6k9 clone submission_tools
+#mv submission_tools/osfstorage/prepare_C2M2_submission.py .
+#mv submission_tools/osfstorage/external_CV_reference_files .
+osf -p c63aw fetch osfstorage/C2M2_datapackage.json
