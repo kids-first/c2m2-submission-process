a4f2a0496a1ae497393998af66ae1db651bb4cbe
William Khan <william@d3b.center>
2023-01-18 15:08:48 -0500

    Updated naming convention for report generated.

--- validate_submission.sh
+++ validate_submission.sh
@@ -2,7 +2,5 @@
 cd frictionless_validation
 
 # Generates the validation report 
-# TODO: Update filename to be dynamic
-# Example name: frictionlessreport_c2m2_submission_year3_q1_v1.txt
-frictionless validate C2M2_datapackage.json > ../frictionless_report.txt
+frictionless validate C2M2_datapackage.json > "../frictionlessreport_c2m2_submission_$1_$2_$3.txt"
  
