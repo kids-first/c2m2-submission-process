6c16fd737b29a3507e77760b80c6404a0cc67094
William Khan <william@d3b.center>
2023-01-30 13:26:21 -0500

    Added path argument to for script to find hard coded tsv's.

--- manual_check.py
+++ manual_check.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import os
 from os import DirEntry
+from argparse import ArgumentParser
+
+def main():
+    args = get_args()
+    data_frame_dict = {}
+    with os.scandir(args.path) as directory:
+        tsv_s = filter(is_tsv,directory)
+        for tsv in tsv_s:
+            print(tsv)
+            data_frame_dict.update({tsv.name : pd.read_table(tsv)})
+
+
+    while user_input := get_table_from_user():
+        user_selected_df = data_frame_dict[user_input + '.tsv']
+        print(user_selected_df.head())
+
 
 def get_table_from_user() -> str:
     user_input = input("What table would you like to see? ")
@@ -10,17 +26,10 @@ def is_tsv(file : DirEntry):
     tsv_check = file.is_file() and file.name.endswith('.tsv')
     return tsv_check
 
-data_frame_dict = {}
-with os.scandir("./frictionless_validation") as directory:
-    tsv_s = filter(is_tsv,directory)
-    for tsv in tsv_s:
-        print(tsv)
-        data_frame_dict.update({tsv.name : pd.read_table(tsv)})
+def get_args():
+    parser = ArgumentParser(description="Tool for quickly checking table heads.")
+    parser.add_argument("--path",dest="path",default=".",help="valid options: path to directory containing tsv's")
+    return parser.parse_args()
 
-
-while user_input := get_table_from_user():
-    user_selected_df = data_frame_dict[user_input + '.tsv']
-    print(user_selected_df.head())
-    
-     
-    
+if __name__ == "__main__":
+    main() 
