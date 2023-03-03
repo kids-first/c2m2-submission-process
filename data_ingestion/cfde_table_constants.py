c0dac05c4d0d4dd5a125609214267c7f858ee444
wnkhan <wnkhan32@gmail.com>
2023-03-03 00:52:42 -0500

    :sparkles: Added subject role taxonomy and cfde table cols dict.

---
+++ data_ingestion/cfde_table_constants.py
@@ -0,0 +1,12 @@
+
+cfde_table_columns_dict = {'subject_role_taxonomy':
+                           ['subject_id_namespace','subject_local_id','role_id','taxonomy_id'],
+                           'biosample_disease':
+                           ['biosample_id_namespace','biosample_local_id','association_type'], #missing disease column
+                           'biosample_from_subject':
+                           ['biosample_id_namespace','biosample_local_id','subject_id_namespace','subject_local_id','age_at_sampling'],
+                           'biosample':
+                           ['id_namespace','local_id','project_id_namespace','project_local_id','created_at','uberon_id_anatomical_site'],
+                           'subject':
+                           ['id_namespace','local_id','project_id_namespace','project_local_id','created_at','gender','ethnicity']
+                           }
--- data_ingestion/transform.py
+++ data_ingestion/transform.py
@@ -2,7 +2,9 @@ import os
 import pandas as pd
 from typing import List
 
+from cfde_table_constants import cfde_table_columns_dict
 from cfde_convert import gender_to_cfde_subject_sex, ethnicity_to_cfde_subject_ethnicity, tissue_type_to_cfde_disease_association
+
 
 ingestion_path = os.path.join(os.getcwd(),'data_ingestion')
 ingested_path = os.path.join(ingestion_path,'ingested') 
@@ -22,6 +24,8 @@ def main():
 
     get_biosample_disease(kf_parts=kf_participants)
 
+    get_subject_role_taxonomy(kf_parts=kf_participants)
+
 
 def get_kf_visible_participants():
     kf_participant_df = pd.read_csv(os.path.join(ingested_path,'participant.csv'))
@@ -35,11 +39,14 @@ def get_kf_visible_participants():
     visible_pariticipants['id_namespace'] = 'kidsfirst:'
     visible_pariticipants['project_id_namespace'] = 'kidsfirst:'
 
-    visible_pariticipants = visible_pariticipants[['id_namespace','kf_id','project_id_namespace','study_id','created_at','gender','ethnicity']]
     visible_pariticipants = gender_to_cfde_subject_sex(visible_pariticipants)
     visible_pariticipants['gender'].fillna('cfde_subject_sex:0',inplace=True)
     visible_pariticipants = ethnicity_to_cfde_subject_ethnicity(visible_pariticipants)
-    visible_pariticipants.sort_values(by=['kf_id'],inplace=True)
+    visible_pariticipants.rename(columns={'kf_id':'local_id','study_id':'project_local_id'},inplace=True) 
+
+    visible_pariticipants = visible_pariticipants[cfde_table_columns_dict.get('subject')]
+    
+    visible_pariticipants.sort_values(by=['local_id'],inplace=True)
     visible_pariticipants.to_csv(os.path.join(transformed_path,'subject.tsv'),sep='\t',index=False)
 
     return visible_pariticipants
@@ -51,12 +58,14 @@ def get_biosample(kf_parts: pd.DataFrame) -> None:
     kf_biospec_df = biospec_df.merge(kf_parts,
                                      how='inner',
                                      left_on='participant_id',
-                                     right_on='kf_id')
+                                     right_on='local_id')
 
     kf_biospec_df['id_namespace'] = id_namespace
     kf_biospec_df['project_id_namespace'] = project_id_namespace
-    kf_biospec_df.rename(columns={'kf_id_x':'local_id','created_at_x':'created_at'},inplace=True)
-    kf_biospec_df = kf_biospec_df[['id_namespace','local_id','project_id_namespace','study_id','created_at','uberon_id_anatomical_site']]
+    kf_biospec_df.rename(columns={'created_at_x':'created_at'},inplace=True)
+
+    kf_biospec_df = kf_biospec_df[cfde_table_columns_dict.get('biosample')]
+
     kf_biospec_df.sort_values(by=['local_id'],ascending=True,inplace=True)
     kf_biospec_df.to_csv(os.path.join(transformed_path,'biosample.tsv'),sep='\t',index=False)
 
@@ -67,7 +76,7 @@ def get_biosample_from_subject(kf_parts: pd.DataFrame) -> None:
     kf_biospec_df = biospec_df.merge(kf_parts,
                                      how='inner',
                                      left_on='participant_id',
-                                     right_on='kf_id')
+                                     right_on='local_id')
 
     kf_biospec_df['biosample_id_namespace'] = 'kidsfirst:'
     kf_biospec_df['subject_id_namespace'] = 'kidsfirst:'
@@ -76,12 +85,12 @@ def get_biosample_from_subject(kf_parts: pd.DataFrame) -> None:
     kf_biospec_df['age_at_event_days'] = kf_biospec_df['age_at_event_days'].astype('float',copy=False)
 
 
-    kf_biospec_df = kf_biospec_df[['biosample_id_namespace','kf_id_x','subject_id_namespace','kf_id_y','age_at_event_days']]
-
-    kf_biospec_df.rename(columns={'kf_id_x':'biosample_local_id',
-                                  'kf_id_y':'subject_local_id',
+    kf_biospec_df.rename(columns={'kf_id':'biosample_local_id',
+                                  'local_id':'subject_local_id',
                                   'age_at_event_days':'age_at_sampling'},
                                   inplace=True)
+
+    kf_biospec_df = kf_biospec_df[cfde_table_columns_dict.get('biosample_from_subject')]
 
     kf_biospec_df.to_csv(os.path.join(transformed_path,'biosample_from_subject.tsv'),sep='\t',index=False)
     
@@ -92,20 +101,31 @@ def get_biosample_disease(kf_parts: pd.DataFrame) -> None:
     kf_biospec_df = biospec_df.merge(kf_parts,
                                      how='inner',
                                      left_on='participant_id',
-                                     right_on='kf_id')
+                                     right_on='local_id')
 
     kf_biospec_df['biosample_id_namespace'] = id_namespace
 
     kf_biospec_df = tissue_type_to_cfde_disease_association(kf_biospec_df)
 
-    kf_biospec_df.rename(columns={'kf_id_x':'biosample_local_id','created_at_x':'created_at','source_text_tissue_type':'association_type'},inplace=True)
+    kf_biospec_df.rename(columns={'kf_id':'biosample_local_id','created_at_x':'created_at','source_text_tissue_type':'association_type'},inplace=True)
 
-    kf_biospec_df = kf_biospec_df[['biosample_id_namespace','biosample_local_id','association_type']]
+    kf_biospec_df = kf_biospec_df[cfde_table_columns_dict.get('biosample_disease')]
 
     kf_biospec_df.sort_values(by=['biosample_local_id'],inplace=True)
     kf_biospec_df.to_csv(os.path.join(transformed_path,'biosample_disease.tsv'),sep='\t',index=False)
 
 
+def get_subject_role_taxonomy(kf_parts: pd.DataFrame):
+    subject_role_taxonomy_df = pd.DataFrame()
+    subject_role_taxonomy_df['subject_local_id'] = kf_parts['local_id']
+    subject_role_taxonomy_df['subject_id_namespace'] = id_namespace
+    subject_role_taxonomy_df['role_id'] = 'cfde_subject_role:0'
+    subject_role_taxonomy_df['taxonomy_id'] = 'NCBI:txid9606'
+
+    subject_role_taxonomy_df = subject_role_taxonomy_df[cfde_table_columns_dict.get('subject_role_taxonomy')]
+
+    subject_role_taxonomy_df.sort_values(by=['subject_local_id'],inplace=True)
+    subject_role_taxonomy_df.to_csv(os.path.join(transformed_path,'subject_role_taxonomy.tsv'),sep='\t',index=False)
 
 def prepare_transformed_directory():
     try:
