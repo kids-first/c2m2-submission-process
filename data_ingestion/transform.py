3a48365f3f9ad551de3e583fa72b809e257eaad9
wnkhan <wnkhan32@gmail.com>
2023-03-09 14:32:50 -0500

    :recycle: Refactor setting constants and column renaming.

--- data_ingestion/transform.py
+++ data_ingestion/transform.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from typing import List
 
-from cfde_table_constants import get_table_cols_from_c2m2_json
+from cfde_table_constants import get_table_cols_from_c2m2_json, get_column_mappings, add_constants
 from cfde_convert import gender_to_cfde_subject_sex, ethnicity_to_cfde_subject_ethnicity, tissue_type_to_cfde_disease_association
 
 
@@ -42,11 +42,10 @@ def get_project():
                                   left_on='kf_id',
                                   right_on='studies_on_portal')
 
-    studies_df['id_namespace'] = id_namespace
-    studies_df['persistent_id'] = ''
-    studies_df['creation_time'] = '' 
+    studies_df = add_constants(studies_df,'project')
     studies_df['abbreviation'] = studies_df['kf_id']
-    studies_df.rename(columns={'kf_id':'local_id','short_name':'name','name':'description'},inplace=True)
+
+    studies_df.rename(columns=get_column_mappings('project'),inplace=True)
 
     studies_df = studies_df[get_table_cols_from_c2m2_json('project')]
 
@@ -63,10 +62,9 @@ def get_project_in_project():
                                   left_on='kf_id',
                                   right_on='studies_on_portal')
 
-    studies_df['parent_project_id_namespace'] = id_namespace
-    studies_df['parent_project_local_id'] = 'drc'
-    studies_df['child_project_id_namespace'] = id_namespace
-    studies_df['child_project_local_id'] = studies_df['kf_id']
+    studies_df = add_constants(studies_df,'project_in_project') 
+
+    studies_df.rename(columns=get_column_mappings('project_in_project'), inplace=True)
 
     studies_df = studies_df[get_table_cols_from_c2m2_json('project_in_project')]
 
@@ -83,20 +81,13 @@ def get_kf_visible_participants():
                                                     left_on='study_id',
                                                     right_on='studies_on_portal')
 
-    visible_pariticipants['id_namespace'] = 'kidsfirst:'
-    visible_pariticipants['project_id_namespace'] = 'kidsfirst:'
-    visible_pariticipants['persistent_id'] = ' '
-    visible_pariticipants['granularity'] = ' '
-    visible_pariticipants['age_at_enrollment'] = ' '
+    visible_pariticipants = add_constants(visible_pariticipants,'subject')
 
     visible_pariticipants = gender_to_cfde_subject_sex(visible_pariticipants)
     visible_pariticipants['gender'].fillna('cfde_subject_sex:0',inplace=True)
     visible_pariticipants = ethnicity_to_cfde_subject_ethnicity(visible_pariticipants)
 
-    visible_pariticipants.rename(columns={'kf_id':'local_id',
-                                          'gender':'sex',
-                                          'study_id':'project_local_id',
-                                          'created_at':'creation_time'},inplace=True) 
+    visible_pariticipants.rename(columns=get_column_mappings('subject'),inplace=True) 
 
     visible_pariticipants = visible_pariticipants[get_table_cols_from_c2m2_json('subject')]
     
@@ -106,6 +97,7 @@ def get_kf_visible_participants():
     return visible_pariticipants
 
 
+#requires additional work for anatomy
 def get_biosample(kf_parts: pd.DataFrame) -> None:
     biospec_df = pd.read_csv(os.path.join(ingested_path,'biospecimen.csv'),low_memory=False)
 
@@ -114,15 +106,12 @@ def get_biosample(kf_parts: pd.DataFrame) -> None:
                                      left_on='participant_id',
                                      right_on='local_id')
 
-    kf_biospec_df['id_namespace'] = id_namespace
-    kf_biospec_df['project_id_namespace'] = project_id_namespace
-    kf_biospec_df['sample_prep_method'] = ' '
+    kf_biospec_df = add_constants(kf_biospec_df,'biosample') 
 
-    kf_biospec_df.drop(columns=['local_id'],inplace=True)
-    kf_biospec_df.rename(columns={'kf_id':'local_id',
-                                  'created_at_x':'created_at',
-                                  'uberon_id_anatomical_site':'anatomy'},
-                                  inplace=True)
+    # Getting rid of conflicting subject cols
+    kf_biospec_df.drop(columns=['local_id','creation_time'],inplace=True)
+
+    kf_biospec_df.rename(columns=get_column_mappings('biosample'),inplace=True)
 
     kf_biospec_df = kf_biospec_df[get_table_cols_from_c2m2_json('biosample')]
 
@@ -142,15 +131,11 @@ def get_biosample_from_subject(kf_parts: pd.DataFrame) -> None:
                                      left_on='participant_id',
                                      right_on='local_id')
 
-    kf_biospec_df['biosample_id_namespace'] = 'kidsfirst:'
-    kf_biospec_df['subject_id_namespace'] = 'kidsfirst:'
+    kf_biospec_df = add_constants(kf_biospec_df,'biosample_from_subject')
     kf_biospec_df['age_at_event_days'] = kf_biospec_df['age_at_event_days'].apply(convert_days_to_years)
 
 
-    kf_biospec_df.rename(columns={'kf_id':'biosample_local_id',
-                                  'local_id':'subject_local_id',
-                                  'age_at_event_days':'age_at_sampling'},
-                                  inplace=True)
+    kf_biospec_df.rename(columns=get_column_mappings('biosample_from_subject'),inplace=True)
 
     kf_biospec_df = kf_biospec_df[get_table_cols_from_c2m2_json('biosample_from_subject')]
     kf_biospec_df.sort_values(by=['biosample_local_id'],inplace=True)
@@ -174,15 +159,11 @@ def get_biosample_disease(kf_parts: pd.DataFrame) -> None:
                                                    right_on='participant_id'
                                                    )
 
-    biosample_disease_df['biosample_id_namespace'] = id_namespace
+    add_constants(biosample_disease_df,'biosample_disease')
 
     biosample_disease_df = tissue_type_to_cfde_disease_association(biosample_disease_df)
 
-    biosample_disease_df.rename(columns={'kf_id':'biosample_local_id',
-                                  'created_at_x':'created_at',
-                                  'source_text_tissue_type':'association_type',
-                                  'DOID':'disease'},
-                                  inplace=True)
+    biosample_disease_df.rename(columns=get_column_mappings('biosample_disease'),inplace=True)
 
     biosample_disease_df = biosample_disease_df[get_table_cols_from_c2m2_json('biosample_disease')]
 
@@ -192,7 +173,6 @@ def get_biosample_disease(kf_parts: pd.DataFrame) -> None:
 
 
 def get_subject_disease(kf_parts: pd.DataFrame) -> None:
-    diagnosis_df = pd.read_csv(os.path.join(ingested_path,'diagnosis.csv'),low_memory=False)
     disease_mapping_df = pd.read_csv(os.path.join(conversion_path,'project_disease_matrix_only.csv'))
 
     kf_parts_diseased = kf_parts.merge(disease_mapping_df,
@@ -201,14 +181,10 @@ def get_subject_disease(kf_parts: pd.DataFrame) -> None:
                                        right_on='study_id').drop(columns=['study_id','DOID_description'])
 
 
-    kf_parts_diseased['subject_id_namespace'] = id_namespace
-    kf_parts_diseased['association_type'] = 'cfde_disease_association_type:1'
+    add_constants(kf_parts_diseased,'subject_disease') 
 
 
-    kf_parts_diseased.rename(columns={'local_id':'subject_local_id',
-                                  'created_at_x':'created_at',
-                                  'DOID':'disease'},
-                                  inplace=True)
+    kf_parts_diseased.rename(columns=get_column_mappings('subject_disease'),inplace=True)
 
     kf_parts_diseased = kf_parts_diseased[get_table_cols_from_c2m2_json('subject_disease')]
 
@@ -218,11 +194,10 @@ def get_subject_disease(kf_parts: pd.DataFrame) -> None:
 
 
 def get_subject_role_taxonomy(kf_parts: pd.DataFrame):
-    subject_role_taxonomy_df = pd.DataFrame()
-    subject_role_taxonomy_df['subject_local_id'] = kf_parts['local_id']
-    subject_role_taxonomy_df['subject_id_namespace'] = id_namespace
-    subject_role_taxonomy_df['role_id'] = 'cfde_subject_role:0'
-    subject_role_taxonomy_df['taxonomy_id'] = 'NCBI:txid9606'
+    subject_role_taxonomy_df = kf_parts.copy(deep=True)
+
+    add_constants(subject_role_taxonomy_df,'subject_role_taxonomy')
+    subject_role_taxonomy_df.rename(columns=get_column_mappings('subject_role_taxonomy'),inplace=True)
 
     subject_role_taxonomy_df = subject_role_taxonomy_df[get_table_cols_from_c2m2_json('subject_role_taxonomy')]
 
