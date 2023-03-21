a0daa6a9baac2545a22e8142d021d96475fe2778
wnkhan <wnkhan32@gmail.com>
2023-03-21 16:24:10 -0400

    :bug: Corrected issue in table joining where data was being lost.

--- data_ingestion/table_ops.py
+++ data_ingestion/table_ops.py
@@ -2,11 +2,6 @@ import pandas as pd
 
 from cfde_table_constants import add_constants, get_column_mappings, get_table_cols_from_c2m2_json
 
-def udpate_primary_key(the_df: pd.DataFrame):
-    the_df.rename({'kf_id_y':'kf_id'},axis='columns',inplace=True)
-    the_df.drop(['kf_id_x'],axis='columns',inplace=True)
-    return the_df
-
 def remove_suffix(label: str):
     if label and label.endswith('_y'):
         return label.removesuffix('_y')
@@ -14,9 +9,6 @@ def remove_suffix(label: str):
         return label
 
 def remove_duplicate_columns(the_df: pd.DataFrame):
-    if 'kf_id_x' in the_df.columns:
-        the_df = udpate_primary_key(the_df)
-
     the_df.drop([col for col in the_df.columns if '_x' in col],axis='columns',inplace=True)
     the_df.rename(remove_suffix,axis='columns',inplace=True)
     return the_df
@@ -27,16 +19,12 @@ class TableJoiner:
     def __init__(self,base_table: pd.DataFrame=None):
         self.base_table=base_table
 
-    def join_table(self, join_table: pd.DataFrame, left_key, right_key=None, join_type=None):
-
-        if not join_type:
-            join_type = 'inner'
-
+    def join_table(self, join_table: pd.DataFrame, left_key, right_key=None):
         merge_kw_args = None
         if right_key:
-            merge_kw_args = {'how':join_type,'left_on':left_key,'right_on':right_key}
+            merge_kw_args = {'how':'inner','left_on':left_key,'right_on':right_key}
         else:
-            merge_kw_args = {'how':join_type,'on':left_key}
+            merge_kw_args = {'how':'inner','on':left_key}
 
         self.base_table = self.base_table.merge(join_table,**merge_kw_args)
 
@@ -44,9 +32,35 @@ class TableJoiner:
 
         return self
 
-    def left_join(self, join_table: pd.DataFrame, left_key, right_key=None):
-        self.join_table(join_table,left_key,right_key,'left')
+    def left_join(self, join_table: pd.DataFrame, left_key, right_key):
+        self.base_table = self.base_table.merge(join_table,
+                                                how='left',
+                                                left_on=left_key,
+                                                right_on=right_key)
+        if 'kf_id_x' in self.base_table.columns:
+            self.update_primary_key(left_key,right_key)
+        
+        self.base_table = remove_duplicate_columns(self.base_table)
+
         return self
+
+    def update_primary_key(self, left_key, right_key):
+        old_primary = left_key if 'kf_id' not in left_key else right_key
+
+        rename_dict = {}
+        # if 'kf_id' in left_key:
+        rename_dict['kf_id_x'], rename_dict['kf_id_y'] = old_primary, 'kf_id'
+        # else:
+        #     rename_dict['kf_id_x'], rename_dict['kf_id_y'] = 'kf_id', old_primary 
+
+        self.base_table.rename(columns=rename_dict,inplace=True)
+
+        drop_cols = [col for col in self.base_table.columns 
+                     if (isinstance(col,str) and 
+                         (col.endswith('_x') or col.endswith('_y')) and
+                          col.startswith(old_primary))]
+
+        self.base_table.drop(drop_cols,axis='columns',inplace=True)
 
     def get_result(self):
         return self.base_table.copy(deep=True)
