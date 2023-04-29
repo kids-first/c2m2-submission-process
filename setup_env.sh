077d5305e50dab7f7a93d51b6b617c4534a660ee
wnkhan <wnkhan32@gmail.com>
2023-04-29 01:22:46 +0000

    :rocket: Updated script that performs venv setup.

--- setup_env.sh
+++ setup_env.sh
@@ -1,8 +1,8 @@
 # Creates a virtual environment in the current directory
-python3 -m venv .
+python3 -m venv venv
 
 # Activates the venv
-source bin/activate
+source /venv/bin/activate
 
 # Installs package dependencies from requirements.txt
 pip install -r requirements.txt
