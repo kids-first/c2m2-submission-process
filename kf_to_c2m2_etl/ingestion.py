57e0c219dcd272ef7d68dd038df48849094950e5
wnkhan <wnkhan32@gmail.com>
2023-04-29 01:16:48 +0000

    :recycle: Removed dependency on KfTableCombiner.

--- kf_to_c2m2_etl/ingestion.py
+++ kf_to_c2m2_etl/ingestion.py
@@ -2,7 +2,6 @@ import os
 import pandas as pd
 from typing import List
 
-from kf_table_combiner import KfTableCombiner
 from pandas_io_util import PandasCsvUpdater, handle_pre_existing_files
 from time_keeper import Timer
 
@@ -13,13 +12,13 @@ targets = ['participants','diagnoses','studies',
              'genomic-files','biospecimen-genomic-files']
 
 project_directory = os.getcwd()
+etl_path = os.path.join(os.getcwd(),'kf_to_c2m2_etl')
 test_ingest_dir = os.path.join(project_directory,'kf_to_c2m2_etl','ingest_test')
 
 timer = Timer()
 
 def main():
-    studies = KfTableCombiner(['portal_studies'])\
-            .get_combined_table()['studies_on_portal'].to_list()
+    studies = pd.read_table(os.path.join(etl_path,'studies_on_portal.tsv'))['studies_on_portal'].to_list()
 
     ingestor = Ingest(studies)
 
@@ -34,6 +33,14 @@ def main():
                 the_df.sort_values(by=['kf_id'],inplace=True)
                 PandasCsvUpdater(endpoint,the_df).update_csv_with_df()
 
+
+def prepare_test_ingest_directory():
+    try:
+        os.mkdir(test_ingest_dir)
+    except:
+        print('Transformed directory already exists.... Skipping directory creation.') 
+
 if __name__ == "__main__":
+    prepare_test_ingest_directory()
     handle_pre_existing_files()
     main()
