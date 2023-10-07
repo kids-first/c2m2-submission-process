8b559666dbc5000fbfa3ca5308789a0bd9d6fc67
wnkhan <wnkhan32@gmail.com>
2023-10-06 22:02:39 -0400

    :sparkles: Added support for csv.

--- submission_diff_inquiry/create_dbs.py
+++ submission_diff_inquiry/create_dbs.py
@@ -3,7 +3,7 @@ import pandas as pd
 import sqlite3
 from argparse import ArgumentParser
 
-def tsvs_to_sqlite(directory: str):
+def tsvs_to_sqlite(file_type: str, directory: str):
     # Derive the database name from the directory name
     root_dir = directory.split('/')[0] if '/' in directory else directory
     db_name = root_dir + '.db'
@@ -11,31 +11,33 @@ def tsvs_to_sqlite(directory: str):
     # Connect to SQLite database
     conn = sqlite3.connect(db_name)
     
-    # List all .tsv files in the directory
-    tsv_files = [f for f in os.listdir(directory) if f.endswith('.tsv')]
+    # List all .csv/.tsv files in the directory
+    data_files = [f for f in os.listdir(directory) if f.endswith('.tsv' if file_type == 'tsv' else '.csv')]
     
-    for tsv_file in tsv_files:
-        # Read the .tsv file into a DataFrame
-        df = pd.read_csv(os.path.join(directory, tsv_file), sep='\t')
+    for data_file in data_files:
+        # Read the .csv/.tsv file into a DataFrame
+        separator = '\t' if file_type == 'tsv' else ','
+        df = pd.read_csv(os.path.join(directory, data_file), sep=separator)
         
         # Use the file name (without extension) as the table name
-        table_name = os.path.splitext(tsv_file)[0]
+        table_name = os.path.splitext(data_file)[0]
         
         # Write the DataFrame to the SQLite database
         df.to_sql(table_name, conn, if_exists='replace', index=False)
-    
+
     # Close the database connection
     conn.close()
 
-def get_directories():
+def get_cli_args():
     parser = ArgumentParser(prog='db Creator',
                             description='Creates dbs from TSVs')
     parser.add_argument("dirs", metavar="DIRECTORIES", type=str, nargs='+',help='a list of directories')
+    parser.add_argument("--file-type", default="tsv", type=str, help='file extension of data tables')
     args = parser.parse_args()
-    return args.dirs
+    return args
 
 # List of directories
-directories = get_directories()  # Replace with your list of directories
+args = get_cli_args()  # Replace with your list of directories
 
-for directory in directories:
-    tsvs_to_sqlite(directory)
+for directory in args.dirs:
+    tsvs_to_sqlite(args.file_type, directory)
