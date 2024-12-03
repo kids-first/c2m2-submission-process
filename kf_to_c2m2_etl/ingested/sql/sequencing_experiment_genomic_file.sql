12725dd6267b186d29d9e6632dbb3c05ab9a1d23
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2024-12-03 14:13:12 -0500

    Update sequencing_experiment_genomic_file.sql
    Exclude CBTN sequencing experiment genomic files

--- kf_to_c2m2_etl/ingested/sql/sequencing_experiment_genomic_file.sql
+++ kf_to_c2m2_etl/ingested/sql/sequencing_experiment_genomic_file.sql
@@ -1,2 +1,6 @@
 select * 
-from sequencing_experiment_genomic_file segf;
+from sequencing_experiment_genomic_file segf
+left join biospecimen_genomic_file on segf.genomic_file_id = biospecimen_genomic_file.genomic_file_id
+left join biospecimen on biospecimen_genomic_file.biospecimen_id = biospecimen.kf_id
+left join participant on biospecimen.participant_id = participant.kf_id
+where participant.study_id != "SD_BHJXBDQK";
