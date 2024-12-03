3c9d83561ea837a4d2bebf1e78cdca1e497855c7
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2024-12-03 14:13:00 -0500

    Update biospecimen_genomic_file.sql
    Exclude CBTN biospecimen genomic files

--- kf_to_c2m2_etl/ingested/sql/biospecimen_genomic_file.sql
+++ kf_to_c2m2_etl/ingested/sql/biospecimen_genomic_file.sql
@@ -1,2 +1,5 @@
-select * 
-from biospecimen_genomic_file;
+select *
+from biospecimen_genomic_file
+left join biospecimen on biospecimen_genomic_file.biospecimen_id = biospecimen.kf_id
+left join participant on biospecimen.participant_id = participant.kf_id
+where participant.study_id != "SD_BHJXBDQK";
