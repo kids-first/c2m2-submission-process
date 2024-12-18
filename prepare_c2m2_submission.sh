e2406da481276132f385df5a97d1932a73ea0646
wnkhan <wnkhan32@gmail.com>
2024-12-18 19:52:24 +0000

    Adds zip command to the  script

--- prepare_c2m2_submission.sh
+++ prepare_c2m2_submission.sh
@@ -20,3 +20,8 @@ cp draft_C2M2_submission_TSVs/*.tsv      frictionless_validation
 # Moves the C2M2 file used to validate the submitted files
 echo "Copying c2m2 schema json to frictionless_validation"
 cp C2M2_datapackage.json                 frictionless_validation
+
+# Zips the C2M2 files into an archive ready for submission
+echo "Zipping the c2m2 files into an archive"
+zip -r "$(date +"%Y%m%d_%H%M%S")_frictionless.zip" frictionless_validation
+
