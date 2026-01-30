2db339b69dcb5562296f7e1ed70972ea2e177215
Christopher Nemarich <cmn2@pm.me>
2026-01-30 16:23:49 -0500

    Create new pipeline file to utilize cfde-c2m2 cli tool

---
+++ src/pipeline.sh
@@ -0,0 +1,19 @@
+
+
+# fill in your tables however you like
+python3 ./src/sql_to_tsv_stream.py
+python3 ./src/loader.py
+
+# create an empty c2m2 submission OR update your existing submission directory
+cd submission
+cfde-c2m2 init
+
+# finish preparing your package by resolving iris
+cd submission
+cfde-c2m2 prepare
+
+# verify integrity of your package
+cfde-c2m2 validate
+
+# zip the necessary files for a bare minimum package
+cfde-c2m2 package
