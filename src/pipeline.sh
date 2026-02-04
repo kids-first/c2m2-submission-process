52e2d39eef090a16976317739efacddd03ebdb17
Christopher Nemarich <cmn2@pm.me>
2026-02-04 17:24:42 -0500

    Specify python version

--- src/pipeline.sh
+++ src/pipeline.sh
@@ -1,15 +1,14 @@
 
 
 # fill in your tables however you like
-python3 ./src/sql_to_tsv_stream.py
-python3 ./src/loader.py
+python3.12 ./src/sql_to_tsv_stream.py
+python3.12 ./src/loader.py
 
 # create an empty c2m2 submission OR update your existing submission directory
 cd submission
 cfde-c2m2 init
 
 # finish preparing your package by resolving iris
-cd submission
 cfde-c2m2 prepare
 
 # verify integrity of your package
--- src/setup_env.sh
+++ src/setup_env.sh
@@ -1,8 +1,8 @@
 # Creates a virtual environment in the current directory
-python3 -m venv venv
+python3.12 -m venv venv
 
 # Activates the venv
 source ./venv/bin/activate
 
 # Installs package dependencies from requirements.txt
-pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
