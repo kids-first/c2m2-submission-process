c17edaad1237d351be01f2648fb6eb0ac0b2679d
Christopher Nemarich <cmn2@pm.me>
2026-02-05 14:56:00 -0500

    Replace `loader.py` script with SQL scripts and upstream dbt changes

---
+++ src/sql/dcc.sql
@@ -0,0 +1,11 @@
+select
+  'cfde_registry_dcc:kidsfirst' as id,
+  'The Gabriella Miller Kids First Pediatric Research Program' as dcc_name,
+  'KFDRC' as dcc_abbreviation,
+  'A large-scale data resource to help researchers uncover new insights into \
+  the biology of childhood cancer and structural birth defects.' as dcc_description,
+  'support@kidsfirstdrc.org' as contact_email,
+  'Kids First Support' as contact_name,
+  'https://kidsfirstdrc.org' as dcc_url,
+  'kidsfirst:' as project_id_namespace,
+  'drc' as project_local_id
---
+++ src/sql/id_namespace.sql
@@ -0,0 +1,6 @@
+select
+  'kidsfirst:' as id,
+  'KFDRC_NS' as abbreviation,
+  'The Gabriella Miller Kids First Pediatric Research Program' as name,
+  'A large-scale data resource to help researchers uncover new insights into \
+  the biology of childhood cancer and structural birth defects.' as description
